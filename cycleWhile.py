837768907f55653846dd112a3f7e60f1454672b7
pault <p.p.tikhomirov@ya.ru>
2024-07-27 16:21:26 +0300

    In the while cycle, the check for matching the length of the list and going beyond its boundary has been removed

--- cycleWhile.py
+++ cycleWhile.py
@@ -1,6 +1,6 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 ind_my_list = 0
-# while len(my_list) >= ind_my_list:
+# while len(my_list) > ind_my_list:
 #     if my_list[ind_my_list] < 0:
 #          print("The end of list, next number negative. Goodbye")
 #          break
@@ -12,9 +12,8 @@ ind_my_list = 0
 #         continue
 
 # Без применения break и я думаю так будет лаконичнее
-while my_list[ind_my_list] >= 0 and len(my_list) >= ind_my_list:
+while len(my_list) > ind_my_list and my_list[ind_my_list] >= 0:
     print(f"The number at index {ind_my_list} => ", my_list[ind_my_list])
-
     if my_list[ind_my_list] > 0:
         print(f"{my_list[ind_my_list]} is a positive number.\n")
         ind_my_list += 1
@@ -22,6 +21,5 @@ while my_list[ind_my_list] >= 0 and len(my_list) >= ind_my_list:
         print("Skipped\n")
         ind_my_list += 1
         continue
-
 print("The end of list, next number negative. Goodbye")
 
