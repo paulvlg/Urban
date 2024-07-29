9630b014487bf6ea7e1f71203eb4e8d01bdec5aa
pault <p.p.tikhomirov@ya.ru>
2024-07-29 21:39:37 +0300

    Solution with the is_prime flag according to the condition

--- cycleFor.py
+++ cycleFor.py
@@ -1,16 +1,29 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-#Для чего он здесь нужен? я не до конца понял.
-is_prime = True
 primes = []
 not_primes = []
 
+#is_prime = True
+# С флагом
 for i in numbers:
+    is_prime = True
     if i > 1:
         for k in range(2, i):
             if i % k == 0:
-                not_primes.append(i)
-                break
-        else:
+                is_prime = False
+        if is_prime:
             primes.append(i)
+        else:
+            not_primes.append(i)
 print(primes)
 print(not_primes)
+
+# Без флага
+# for i in numbers:
+#     if i > 1:
+#         for k in range(2, i):
+#             if i % k == 0:
+#                 not_primes.append(i)
+#                 break
+#         else:
+#             primes.append(i)
+
