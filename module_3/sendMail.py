29b81bac3af75b86dd645d15a43492c0c040eff9
pault <p.p.tikhomirov@ya.ru>
2024-08-01 12:31:56 +0300

    Change the way to check validation email. Added solution for task in Module 3.2 "Send emails"

--- module_3/sendMail.py
+++ module_3/sendMail.py
@@ -28,9 +28,9 @@ def send_email(message, recipient, *,sender="university.help@gmail.com"):
     return
 
 
-# send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-# send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-# send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-# send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
-send_email('Check validation email - without name', '@gmail.com', sender='g@gmail.com')
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
 
