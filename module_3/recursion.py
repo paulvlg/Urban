9355d3010e6aa575e11f51295c00b457e72f72ee
pault <p.p.tikhomirov@ya.ru>
2024-08-06 22:54:46 +0300

    Final solution for task"recursion"  Module 3.5

--- module_3/recursion.py
+++ module_3/recursion.py
@@ -1,10 +1,10 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-    first = int(str_number[0])
-    if len(str_number) < 1:
-        return first
+    if len(str_number) <= 1:
+        return int(str_number)
     else:
+        first = int(str_number[0])
         return first * get_multiplied_digits(int(str_number[1:]))
 
-result = get_multiplied_digits(4000203)
+result = get_multiplied_digits(40203)
 print(result)
