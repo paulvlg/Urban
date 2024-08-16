2823075932025490ae6dcfad05f3639dc424eb15
pault <p.p.tikhomirov@ya.ru>
2024-08-16 12:10:20 +0300

    Added solution - module_4_1.py

--- module_4/fake_math.py
+++ module_4/fake_math.py
@@ -1,6 +1,6 @@
 def divide(first, second):
     if second == 0:
-        res_ = f'Делитель равен {second} - Ошибка'
+        res_ = f'Делитель равен {second} - Результат Ошибка'
     else:
         res_ = f'Результат деления: - {first / second}'
     return res_
