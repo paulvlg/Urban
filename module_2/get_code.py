a0ca30c7c202fb03a52d87e87b0035d6deafa1ef
Pavel T. <strongy@yandex.ru>
2024-08-17 22:25:57 +0300

    refactor

--- module_2/get_code.py
+++ module_2/get_code.py
@@ -5,6 +5,7 @@ def get_decode(number):
         return
     for number1 in range(1, number + 1):
         decode = []
+
         for number2 in range(number1, number + 1):
             if number % (number1 + number2) == 0 and number1 != number2:
                 pairs.append(number1)
