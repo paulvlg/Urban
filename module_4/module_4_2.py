1cc8317982f7cf9f0e1475f4a6001cd39aec0323
Pavel T. <strongy@yandex.ru>
2024-08-17 23:34:55 +0300

    Solution for module_4_2

--- module_4/module_4_2.py
+++ module_4/module_4_2.py
@@ -3,5 +3,6 @@ def test_function():
         print('Я в области видимости функции test_function')
     inner_function()
 
-
-#inner_function()
+test_function()
+#inner_function() #Функция не определена глобально, поэтому будет ошибка -
+# "NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?"
