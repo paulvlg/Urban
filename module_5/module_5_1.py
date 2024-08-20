8e5090d81f251805105bfde6761262ae332ccbae
pault <p.p.tikhomirov@ya.ru>
2024-08-20 13:03:41 +0300

    added checking for values outside ranges

--- module_5/module_5_1.py
+++ module_5/module_5_1.py
@@ -1,18 +1,24 @@
+from typing import get_origin
+
+
 class House:
     def __init__(self, name, floors):
         self.name = name
         self.floors = floors
 
     def go_to(self, new_floor):
-        if new_floor > self.floors:
-            return print(f'Такого этажа ({new_floor}), в доме "{self.name}" - не существует в этом доме.')
-        else:
+        if 0 < new_floor <= self.floors:
             for _ in range(new_floor):
                 print(_ + 1)
-        return print(f'\n{_ + 1} - Вы прибыли на нужный этаж\n')
+            return print(f'\nВы прибыли на нужный этаж - {_ + 1}\n')
+        else:
+            return print(f'Такого этажа ({new_floor}), в доме "{self.name}" - не существует.')
 
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
+h1.go_to(-3)
+h2.go_to(0)
+h1.go_to(20)
