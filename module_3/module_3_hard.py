d8185c6f349a1c5d2595e9c753f7c465a5b5fd99
pault <p.p.tikhomirov@ya.ru>
2024-08-22 01:16:44 +0300

    Again optimization according tutor recommendation and now correct sequence

--- module_3/module_3_hard.py
+++ module_3/module_3_hard.py
@@ -6,20 +6,20 @@ def calculate_structure_sum(*args):
                 total += calculate_structure_sum(i)
         elif isinstance(arg, int):
             total += arg
+        elif isinstance(arg, str):
+            total += len(arg)
         elif isinstance(arg, dict):
             for key, value in arg.items():
                 total += calculate_structure_sum(key, value)
-        elif isinstance(arg, str):
-            total += len(arg)
-        return total
+    return total
 
 
 data_structure = [
-[1, 2, 3],
-{'a': 4, 'b': 5},
-(6, {'cube': 7, 'drum': 8}),
-"Hello",
-((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 result = calculate_structure_sum(data_structure)
 print(result)
