f9fba86ee5609b21c094e27813f9589ac7be7531
pault <p.p.tikhomirov@ya.ru>
2024-08-29 13:19:47 +0300

    Решение для модуля 5.4

--- module_5/module_5_4.py
+++ module_5/module_5_4.py
@@ -1,20 +1,9 @@
-# class Example:
-#   def __new__(cls, *args, **kwargs):
-#     print(args)
-#     print(kwargs)
-#     return object.__new__(cls)
-#
-#   def __init__(self, first, second, third):
-#     print(first)
-#     print(second)
-#     print(third)
-#
-# ex = Example('data', second=25, third=3.14)
-
 class House:
-    class_history = []
+    houses_history = []
+
     def __new__(cls, *args, **kwargs):
-        
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
 
     def __init__(self, name, floors):
         self.name = name
@@ -40,6 +29,8 @@ class House:
         else:
             print('Неверный тип данных')
 
+    def __del__(self):
+        return print(f'{self.name} снесли, но он остался в истории')
 
     def __lt__(self, other):
         if isinstance(self.floors, int) or isinstance(House, House):
@@ -83,25 +74,14 @@ class House:
 
 
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
+# Удаление объектов
+del h2
+del h3
 
-print(h1 == h2)  # __eq__
-
-h1 = h1 + 10  # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10  # __iadd__
-print(h1)
-
-h2 = 10 + h2  # __radd__
-print(h2)
-
-print(h1 > h2)  # __gt__
-print(h1 >= h2)  # __ge__
-print(h1 < h2)  # __lt__
-print(h1 <= h2)  # __le__
-print(h1 != h2)  # __ne__
+print(House.houses_history)
