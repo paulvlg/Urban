b8eb11aa85fb1b671b8a57550b2227017efbc932
pault <p.p.tikhomirov@ya.ru>
2024-09-01 16:36:17 +0300

    added hash func to class User

--- module_5/moduleFinal/User.py
+++ module_5/moduleFinal/User.py
@@ -1,6 +1,20 @@
 class User:
+    """
+    Класс пользователя, содержащий атрибуты: логин, хэш пароля, возраст
+    """
+
     def __init__(self, nickname, password, age):
         self.nickname = nickname
-        self.password = password
+        self.password = hash(password)
         self.age = age
 
+    def __hash__(self):
+        return hash(self.password)
+
+
+class Database:
+    def __init__(self):
+        self.data = {}
+
+    def add_user(self, nickname, password):
+        self.data[nickname] = password
