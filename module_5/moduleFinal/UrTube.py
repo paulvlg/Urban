3996165743336b855458a38ebfebbd21bacf273d
pault <p.p.tikhomirov@ya.ru>
2024-09-03 19:04:49 +0300

    добавлены методы логин, логаут, адд, регистрация.

--- module_5/moduleFinal/UrTube.py
+++ module_5/moduleFinal/UrTube.py
@@ -1,20 +1,60 @@
+import time
+
+
+class User:
+    """
+    Класс пользователя, содержащий атрибуты: логин, хэш пароля, возраст
+    """
+
+    def __init__(self, nickname: str, password: str, age: int):
+        self.nickname = nickname
+        self.password = hash(password)
+        self.age = age
+
+    def __hash__(self):
+        return hash(self.password)
+
+    def __str__(self):
+        return f"Пользователь: {self.nickname}, возраст: {self.age}"
+
+
+class Video:
+    def __init__(self, title: str, duration: int, adult_mode = bool(False)):
+        self.title = title
+        self.duration = duration
+        self.time_now = 0
+        # self.adult_mode = False
+
+
 class UrTube:
-    def __init__(self, users, videos, current_user):
-        self.users = users
-        self.videos = videos
-        self.current_user = current_user
+    def __init__(self):
+        self.users = []
+        self.videos = []
+        self.current_user = None
 
     def log_in(self, nickname, password):
-        pass
+        for user in self.users:
+            if user == nickname and password == user.password:
+                self.current_user = user
+                return
+
 
     def register(self, nickname, password, age):
-        pass
+        password = hash(password)
+        for user in self.users:
+            if nickname == user.nickname:
+                print(f'Пользователь {user} уже существует.')
+                return
+            new_user = User(nickname, password, age)
+            self.users.append(new_user)
+            self.current_user = new_user
+        print(f'Пользователь: {nickname} зарегистрирован и авторизовался в сервисе.')
 
     def log_out(self):
-        pass
+        self.current_user = None
 
-    def add(self, *args, **kwargs):
-        pass
+    def add(self, *args):
+
 
     def get_video(self, *args, **kwargs):
         pass
