e97282f05562ca9069a175240e5c0a63a3540802
pault <p.p.tikhomirov@ya.ru>
2024-09-18 12:40:20 +0300

    По рекомендации преподавателей, вызов super() был изменен на именной вызов родительских классов. Метод get_pos возвращается кортеж, вместо строки.

--- module_6/module_6_3.py
+++ module_6/module_6_3.py
@@ -2,7 +2,6 @@ class Horse:
     def __init__(self):
         self.x_distance = 0
         self.sound = 'Frr'
-        super().__init__()
 
     def run(self, dx: int):
         self.x_distance += dx
@@ -19,14 +18,15 @@ class Eagle:
 
 class Pegasus(Horse, Eagle):
     def __init__(self):
-        super().__init__()
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
         self.run(dx)
         self.fly(dy)
 
     def get_pos(self):
-        return f'Текущая позиция: {self.x_distance}, {self.y_distance}'
+        return self.x_distance, self.y_distance
 
     def voice(self):
         print(self.sound)
