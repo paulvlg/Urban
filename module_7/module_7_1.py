203b15d932aebb823802f95aed74079fc30d8726
pault <p.p.tikhomirov@ya.ru>
2024-09-30 11:54:01 +0300

    Решение для задачи Модуля 7.1

---
+++ module_7/module_7_1.py
@@ -0,0 +1,42 @@
+class Product:
+    def __init__(self, name: str, weight: float, category: str):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        content = file.read()
+        file.close()
+        return content
+
+    def add(self, *products):
+        file = open(self.__file_name, 'a')
+        if not any([product.name in self.get_products() for product in products]):
+            for product in products:
+                file.write(f'{product}\n')
+        else:
+            for product in products:
+                print(f'Продукт {product.name} уже есть в магазине')
+        file.close()
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)  # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
+
